a13cb0419505b5557b932efd411dff8c372c4d2d
Subhrajyoti <subha.ionic3@gmail.com>
2020-07-22 01:45:12 +0530

    worked on storing data in userdefaults

--- Project_10_11_12_Combine/ViewController.swift
+++ Project_10_11_12_Combine/ViewController.swift
@@ -18,6 +18,17 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate & U
         title = "Face Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+        
+        let jsonDecoder = JSONDecoder()
+        let userDefaults = UserDefaults.standard
+        if let savedData = userDefaults.object(forKey: "faces") as? Data {
+            do {
+                faceArray = try jsonDecoder.decode([Face].self, from: savedData)
+            } catch {
+                print("Failed to load faces")
+            }
+        }
+        
     }
     
     @objc func addTapped() {
@@ -41,15 +52,41 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate & U
             try? jpegData.write(to: imagePath)
         }
         
-        faceArray.append(Face(name: imageName, image: imageName))
+        let face = Face(name: imageName, image: imageName)
+        
+        faceArray.append(face)
         tableView.reloadData()
+        save()
+        
+        let ac = UIAlertController(title: "Remane face", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+            guard let safeText = ac?.textFields?[0].text else { return }
+            face.name = safeText
+            self?.tableView.reloadData()
+            self?.save()
+        })
         
         dismiss(animated: true)
+        present(ac, animated: true)
     }
     
     func getDocumentsDirectory() -> URL {
         let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return path[0]
+    }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        
+        if let savedDate = try? jsonEncoder.encode(faceArray) {
+            let userDefaults = UserDefaults.standard
+            userDefaults.set(savedDate, forKey: "faces")
+        } else {
+            print("Failed to save people.")
+        }
     }
 
     
